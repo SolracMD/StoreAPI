4f70d649d7239127d9113457b3b31e9095f0e53b
SolracMD <cmonterrosad@hotmail.com>
2018-08-16 16:43:39 -0600

    fix register endpoint accessLevel

--- routes.js
+++ routes.js
@@ -10,7 +10,7 @@ module.exports = (app) => {
 
   app.post('/api/Products',middleware.authorization, middleware.accessLevel('1,2'), ProductController.Add);
 
-  app.patch('/api/Products/:id', middleware.authorization, middleware.accessLevel('0,1,2'), ProductController.ChangePrice);
+  app.patch('/api/Products/:id', middleware.authorization, middleware.accessLevel('1,2'), ProductController.ChangePrice);
 
   app.post('/api/Products/:id', middleware.authorization, middleware.accessLevel('0,1,2'), ProductController.BuyProduct);
 
@@ -18,9 +18,11 @@ module.exports = (app) => {
 
   app.put('/api/Products/:id', middleware.authorization, middleware.accessLevel('0,1,2'), ProductController.LikeProduct);
 
-  app.post('/api/users', UserController.create);
+  app.post('/api/users', middleware.accessLevel('-1,0,1,2'), UserController.create);
 
-  app.post('./api/users/login', UserController.Login);
+  app.post('/api/users/admin',middleware.authorization, middleware.accessLevel('2'),UserController.create);
+
+  app.post('/api/users/login', UserController.Login);
 
   app.get('/api/Products', ProductController.getProducts);
 };
