36d2f5f10b1415589378fb07fc22b14ad58f20fd
SolracMD <cmonterrosad@hotmail.com>
2018-08-16 11:11:35 -0600

    changePrice logic added

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,5 @@
 const products = require('../models/products');
+const ChangePriceLog = require('../models/changePriceLog');
 
 const Add = (req, res) => {
     const { name, stock, price } = req.body;
@@ -15,6 +16,23 @@ const Add = (req, res) => {
       })
       .catch((err) => {
         res.status(400).send(err);
+      });
+  };
+  const ChangePrice = (req, res) => {
+    const { id } = req.params;
+    const { price } = req.body;
+    const { User } = req.user;
+    //  check for valid prodcutID, update price and add the new price to the log
+    products.findOneAndUpdate({ _id: id }, { price },{new:true}).exec()
+      .then((ProductUpdated) => {
+        res.status(200).send(ProductUpdated);
+        return ChangePriceLog.create({ userID: User._id, productID: id, price });
+      })
+      .then((LogCreated) => {
+        ///c
+      })
+      .catch((err) => {
+        res.send(err);
       });
   };
 
